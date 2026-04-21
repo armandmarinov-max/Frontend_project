ec26f22cfde7525f135d467b05dc8d952b4160fe
Armand Marinov <armand.marinov@student.sintlievenscollege.be>
2026-04-21 14:30:21 +0200

    nieuwe post methode in speler

--- spelers.js
+++ spelers.js
@@ -23,3 +23,38 @@ document.getElementById("load-spelers").addEventListener("click", function()
         })
         .catch(error => console.error('Error fetching data:', error));
 });
+// hier is de post functie om een speler toe te voegen
+document.getElementById("add-speler").addEventListener("click", function()
+{
+    const nieuweSpeler = {
+        voornaam: document.getElementById("voornaam").value,
+        achternaam: document.getElementById("achternaam").value,
+        fide_id: document.getElementById("fide_id").value,
+        land: document.getElementById("land").value,
+        geslacht: document.getElementById("geslacht").value,
+        geboortejaar: document.getElementById("geboortejaar").value
+    };
+
+    fetch('localhost:5080/api/Speler', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(nieuweSpeler)
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Fout bij het toevoegen van de speler');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Speler toegevoegd:', data);
+            alert(`Speler ${data.voornaam} ${data.achternaam} succesvol toegevoegd!`);
+        })
+        .catch(error => console.error('Error posting data:', error));
+});
+
+
+
+                
